e3b0e6048bbd496b153412e77f9e4c7e01ce3d96
Alexander Senetcky <41960163+asenetcky@users.noreply.github.com>
2025-06-12 15:18:54 -0400

    feat: parse table type from url

--- src/dataops/data/csv.py
+++ src/dataops/data/csv.py
@@ -3,7 +3,7 @@ from pathlib import Path
 import polars as pl
 
 
-def csv_wrangle(targetfile: Path) -> pl.LazyFrame:
+def csv_wrangle(targetfile: Path | str) -> pl.LazyFrame:
     """Wrangle starter .csv for creating api endpoints and features.
 
     Args:
@@ -17,6 +17,9 @@ def csv_wrangle(targetfile: Path) -> pl.LazyFrame:
         IOError: For general I/O related errors.
 
     """
+    if not isinstance(targetfile, Path):
+        targetfile = Path(targetfile)
+
     if not Path.exists(targetfile):
         raise FileNotFoundError(f"File path does not exist: {targetfile}")
 
@@ -28,13 +31,28 @@ def csv_wrangle(targetfile: Path) -> pl.LazyFrame:
     ucgid_pattern = r"&ucgid=(.*)$"
     domain_pattern = r"^(.+gov)"
 
-    targets = pl.scan_csv(targetfile).with_columns(
-        pl.col("url").str.slice(28, 4).cast(pl.Int16).alias("year"),
-        pl.col("url").str.slice(40, 1).cast(pl.Int16).alias("acs_type"),
-        pl.col("url").str.extract(group_pattern).alias("group"),
-        pl.col("url").str.extract(ucgid_pattern).alias("ucgid"),
-        pl.col("url").str.extract(domain_pattern).alias("domain"),
-        pl.col("url").str.replace(domain_pattern, "").alias("end"),
+    targets = (
+        pl.scan_csv(targetfile)
+        .with_columns(
+            pl.col("url").str.slice(28, 4).cast(pl.Int16).alias("year"),
+            pl.col("url").str.slice(40, 1).cast(pl.Int16).alias("acs_type"),
+            pl.col("url").str.extract(group_pattern).alias("group"),
+            pl.col("url").str.extract(ucgid_pattern).alias("ucgid"),
+            pl.col("url").str.extract(domain_pattern).alias("domain"),
+            pl.col("url").str.replace(domain_pattern, "").alias("end"),
+        )
+        .with_columns(
+            pl.when(pl.col("group").str.starts_with("B"))
+            .then(pl.lit("detailed"))
+            .when(pl.col("group").str.starts_with("C"))
+            .then(pl.lit("collapsed"))
+            .when(pl.col("group").str.starts_with("S"))
+            .then(pl.lit("subject"))
+            .when(pl.col("group").str.starts_with("DP"))
+            .then(pl.lit("data-profile"))
+            .otherwise(pl.lit("unknown"))
+            .alias("table_type"),
+        )
     )
 
     return targets
