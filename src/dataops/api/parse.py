0ba2740198858cee8ac9bbfae972c8b5a94f5e1f
Alexander Senetcky <41960163+asenetcky@users.noreply.github.com>
2025-06-12 16:23:45 -0400

    feat: use parse_url for parse_csv

--- src/dataops/api/parse.py
+++ src/dataops/api/parse.py
@@ -75,31 +75,6 @@ def parse_csv(targetfile: Path | str) -> pl.LazyFrame:
     if not targetfile.suffix == ".csv":
         raise IOError(f"Expected a csv file. Found file type: {targetfile.suffix}")
 
-    # regex patterns
-    group_pattern = r"\((.*)\)"
-    ucgid_pattern = r"&ucgid=(.*)$"
-    domain_pattern = r"^(.+gov)"
+    targeturl = pl.scan_csv(targetfile).select("url").to_series().to_list()
 
-    targetfile = pl.scan_csv(targetfile).select("url").to_series().to_list()
-
-    targets = targetfile.with_columns(
-        pl.col("url").str.slice(28, 4).cast(pl.Int16).alias("year"),
-        pl.col("url").str.slice(40, 1).cast(pl.Int16).alias("acs_type"),
-        pl.col("url").str.extract(group_pattern).alias("group"),
-        pl.col("url").str.extract(ucgid_pattern).alias("ucgid"),
-        pl.col("url").str.extract(domain_pattern).alias("domain"),
-        pl.col("url").str.replace(domain_pattern, "").alias("end"),
-    ).with_columns(
-        pl.when(pl.col("group").str.starts_with("B"))
-        .then(pl.lit("detailed"))
-        .when(pl.col("group").str.starts_with("C"))
-        .then(pl.lit("collapsed"))
-        .when(pl.col("group").str.starts_with("S"))
-        .then(pl.lit("subject"))
-        .when(pl.col("group").str.starts_with("DP"))
-        .then(pl.lit("data-profile"))
-        .otherwise(pl.lit("unknown"))
-        .alias("table_type"),
-    )
-
-    return targets
+    return parse_url(targeturl)
