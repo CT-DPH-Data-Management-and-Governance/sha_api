26ba4572d52d4ce30a5a8cfcce272e9f1315f622
asenetcky <me@asenetcky.dev>
2025-06-17 20:47:27 -0400

    feat: refine long dataframe cleaning

--- src/dataops/models.py
+++ src/dataops/models.py
@@ -252,50 +252,91 @@ class CensusAPIEndpoint(BaseModel):
         from the census api endpoint and return
         as a polars dataframe.
         """
+
         labels = self.fetch_variable_labels().lazy()
         data = self.fetch_data_to_polars().lazy()
 
         tidy = (
-            data.join(
-                labels,
-                left_on="headers",
-                right_on="name",
-                how="left",
+            (
+                data.join(
+                    labels,
+                    left_on="headers",
+                    right_on="name",
+                    how="left",
+                )
+                .with_columns(
+                    pl.col("label")
+                    .str.replace_all("!|:", " ")
+                    .str.replace_all(r"\s+", " ")
+                    .str.strip_chars()
+                    .str.to_lowercase(),
+                    pl.col("concept").str.to_lowercase(),
+                )
+                .with_columns(
+                    pl.concat_str(
+                        [pl.col("concept"), pl.col("label")], separator=" "
+                    ).alias("variable_name")
+                )
+                .with_columns(
+                    pl.when(pl.col("headers") == "NAME")
+                    .then(pl.lit("name"))
+                    .when(pl.col("headers") == "GEO_ID")
+                    .then(pl.lit("geoid"))
+                    .otherwise(pl.col("variable_name"))
+                    .alias("variable_name"),
+                    pl.col("records").cast(pl.Float32, strict=False).alias("value"),
+                )
+                .with_columns(
+                    pl.col("variable_name").fill_null(strategy="forward"),
+                    pl.col("headers")
+                    .str.slice(-2)
+                    .str.replace_all(r"\d", "")
+                    .alias("value_type"),
+                    pl.col("headers").alias("variable_id"),
+                )
+                .filter(pl.col("value") > -555555555)  # drop suppressed rows
+                .drop_nulls(pl.col("value"))  # drops rows like "***" or (X) post-cast
+                .with_row_index(name="row_id")
+                .with_columns(
+                    pl.col("variable_name")
+                    .str.replace(self.concept, "")
+                    .str.replace("estimates", "")
+                    .str.strip_chars()
+                    .alias("variable_name"),
+                    pl.when(pl.col("value_type") == pl.lit("E"))
+                    .then(pl.lit("estimate"))
+                    .when(pl.col("value_type") == pl.lit("M"))
+                    .then(pl.lit("margin_of_error"))
+                    .when(pl.col("value_type") == pl.lit("P"))
+                    .then(pl.lit("percent_estimate"))
+                    .when(pl.col("value_type") == pl.lit("PM"))
+                    .then(pl.lit("percent_margin_of_error"))
+                    .when(pl.col("value_type") == pl.lit("N"))
+                    .then(pl.lit("count"))
+                    .otherwise(pl.lit("unknown"))
+                    .alias("value_type"),
+                )
+                .select(
+                    [
+                        "row_id",
+                        "variable_id",
+                        "variable_name",
+                        "value",
+                        "value_type",
+                    ]
+                )
             )
-            .with_columns(
-                pl.col("label")
-                .str.replace_all("!|:", " ")
-                .str.replace_all(r"\s+", " ")
-                .str.strip_chars()
-                .str.to_lowercase(),
-                pl.col("concept").str.to_lowercase(),
+            .collect()
+            .select(
+                pl.col("row_id"),
+                pl.lit(self.dataset).alias("dataset"),
+                pl.lit(self.year).alias("year"),
+                pl.col("variable_id"),
+                pl.col("variable_name"),
+                pl.col("value"),
+                pl.col("value_type"),
             )
-            .with_columns(
-                pl.concat_str(
-                    [pl.col("concept"), pl.col("label")], separator=" "
-                ).alias("variable_name")
-            )
-            .with_columns(
-                pl.when(pl.col("headers") == "NAME")
-                .then(pl.lit("name"))
-                .when(pl.col("headers") == "GEO_ID")
-                .then(pl.lit("geoid"))
-                .otherwise(pl.col("variable_name"))
-                .alias("variable_name"),
-                pl.col("records").cast(pl.Float32, strict=False).alias("value"),
-            )
-            .with_columns(
-                pl.col("variable_name").fill_null(strategy="forward"),
-                pl.col("headers")
-                .str.slice(-2)
-                .str.replace_all(r"\d", "")
-                .alias("value_type"),
-            )
-            .filter(pl.col("value") > -555555555)
-            .drop_nulls(pl.col("value"))
-            .with_row_index(name="id")
-            .select(["id", "variable_name", "value", "value_type"])
-        ).collect()
+        )
         return tidy
 
 
