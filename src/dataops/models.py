6708cf256d0dc45d7b0f1a88963c05071da1d739
Alexander Senetcky <41960163+asenetcky@users.noreply.github.com>
2025-07-10 12:01:42 -0400

    tidy: no env vars required now

--- src/dataops/models.py
+++ src/dataops/models.py
@@ -31,13 +31,13 @@ class ApplicationSettings(BaseSettings):
 
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
 
-    census_api_key: str = Field(..., env="CENSUS_API_KEY")
-    domain: str = Field(..., env="DOMAIN")
-    source_id: str = Field(..., env="SOURCE_ID")
-    target_id: str = Field(..., env="TARGET_ID")
-    socrata_user: str = Field(..., env="SOCRATA_USER")
-    socrata_pass: str = Field(..., env="SOCRATA_PASS")
-    socrata_token: str = Field(..., env="SOCRATA_TOKEN")
+    census_api_key: str = Field("", env="CENSUS_API_KEY")
+    domain: str = Field("", env="DOMAIN")
+    source_id: str = Field("", env="SOURCE_ID")
+    target_id: str = Field("", env="TARGET_ID")
+    socrata_user: str = Field("", env="SOCRATA_USER")
+    socrata_pass: str = Field("", env="SOCRATA_PASS")
+    socrata_token: str = Field("", env="SOCRATA_TOKEN")
 
 
 class CensusAPIEndpoint(BaseModel):
