7ba156789b41745fe56f95059b3b3fc54393f1c6
asenetcky <me@asenetcky.dev>
2025-07-17 20:54:09 -0400

    fix: proper type

--- src/dataops/_helpers.py
+++ src/dataops/_helpers.py
@@ -4,7 +4,7 @@ import polars as pl
 def _ensure_column_exists(
     data: pl.LazyFrame | pl.DataFrame,
     column_name: list[str],
-    default_value: any = "unknown as queried",
+    default_value: str = "unknown as queried",
 ) -> pl.LazyFrame:
     for col_name in column_name:
         if col_name not in data.collect_schema().names():
